2638292b87198bd16a00e9250c65815d3a08ace3
Piotr Przybylski <piotrprz@gmail.com>
2015-07-26 01:02:47 +0200

    Script

---
+++ webtoons.com_scroll_killer.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         webtoons.com scroll killer
+// @namespace    https://github.com/Crack/webtoons.com-scroll-killer
+// @description  Restores normal behaviour of down and up keys on webtoons.com
+// @version      1.0
+// @match        http://www.webtoons.com/*
+// @grant        none
+// ==/UserScript==
+
+movePrevious=function(){}
+moveNext=function(){}
